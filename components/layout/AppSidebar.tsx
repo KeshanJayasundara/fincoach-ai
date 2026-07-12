78a5b6bcabf3a34555c67409e81a55ae20ded161
Nipuna Keshan <nipunakeshan2@gmail.com>
2026-07-12 20:08:01 +0530

    fix: logout redirect to /login

--- components/layout/AppSidebar.tsx
+++ components/layout/AppSidebar.tsx
@@ -73,7 +73,7 @@ export default function AppSidebar({ isOpen, onClose }: AppSidebarProps) {
   const handleLogout = async () => {
     onClose();
     await signOut({
-      callbackUrl: "/login",
+      callbackUrl: "/",
       redirect: true,
     });
   };
